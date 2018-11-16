b9e2418132ca89b8617384d8361a2e8e0bfefc8d
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-16 12:13:52 -0800

    Write specs for User model

--- capybara/app/models/user.rb
+++ capybara/app/models/user.rb
@@ -1,2 +1,33 @@
 class User < ApplicationRecord
+
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+
+  end
+
+  def is_password?
+  end
+
+  def ensure_session_token
+  end
+
+  def reset_session_token!
+  end
+
+  def self.find_by_credentials(username, password)
+  end
+
+  private
+
+  def generate_session_token
+  end
 end
--- capybara/spec/models/user_spec.rb
+++ capybara/spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.create!(username: 'jon', password: 'password')}
+
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_length_of(:password).is_at_least(6) }
+
+    it { should validate_uniqueness_of(:username) }
+
+    context 'session_token tests' do
+      it 'assigns the session_token if one is not given' do
+        savio = User.create(username: 'savio', password: 'password')
+        expect(savio.session_token).not_to be_nil
+      end
+    end
+
+    context 'password digest' do
+      it 'assigns password digest given a password' do
+        expect(user.password_digest).not_to be_nil
+        expect(user.password_digest).not_to eq('password')
+      end
+
+      it 'does not store the password' do
+        expect(user.password).to be_nil
+      end
+    end
+
+  end
+
+  describe '::find_by_credentials' do
+    it 'finds a user' do
+      user2 = User.find_by_credentials('jon', 'password')
+      expect(user.id).to eq(user2.id)
+    end
+  end
 end
