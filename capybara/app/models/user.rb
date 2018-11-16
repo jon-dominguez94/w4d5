cd43b160127db21661540335128f0c7222097027
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-16 12:21:06 -0800

    Write user methods

--- capybara/app/models/user.rb
+++ capybara/app/models/user.rb
@@ -11,23 +11,35 @@ class User < ApplicationRecord
 
   def password=(password)
     @password = password
-
+    self.password_digest = BCrypt::Password.create(password)
   end
 
-  def is_password?
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def ensure_session_token
+    self.session_token ||= generate_session_token
   end
 
   def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
   end
 
   def self.find_by_credentials(username, password)
+    user = User.find_by(username: username, password: password)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
   end
 
   private
 
   def generate_session_token
+    SecureRandom.urlsafe_base64
   end
 end
